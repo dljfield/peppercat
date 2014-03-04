4137b0f7cebe25fa236c50f490bbdfa5ea5b99ec
David Field <dvf9@aber.ac.uk>
2014-03-04 13:39:05 +0000

    Rendering existing assets from the server. Set up ability to send JSON from the server.

---
+++ code/routes.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, jsonify, send_file, send_from_directory
+
+app = Flask(__name__, static_folder = 'client')
+
+@app.route('/')
+def index():
+  return render_template('index.html')
+
+@app.route('/scene/<path:scene>')
+def scene(scene):
+	return send_from_directory('client/data', scene + '.json')
+
+if __name__ == '__main__':
+  app.run(debug=True)
