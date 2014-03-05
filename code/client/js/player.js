07d6964005dea125649594a944a65abab4c37fb9
David Field <dvf9@aber.ac.uk>
2014-03-05 13:08:49 +0000

    doin stuff I don't even know

--- code/client/js/player.js
+++ code/client/js/player.js
@@ -44,52 +44,6 @@ var Player = Character.extend({
 		}
 	},
 
-	updatePosition: function(scene, position) {
-
-		// if we've been given a new position, get a path for it and set the destination to the first node
-		if (scene && position) {
-			this.path = this.findPath(scene, position);
-			if (!this.destination)
-				this.destination = this.path.shift();
-		}
-
-		// if we've moved to the current destination we need to get the next node on the list
-		if (this.destination && this.x === this.destination.x && this.y === this.destination.y) {
-			this.destination = this.path.shift();
-		}
-
-		// if we have a destination, move the player towards it based on the speed
-		if (this.destination) {
-			if (this.x < this.destination.x) {
-				if (this.destination.x - this.x <= this.speed) {
-					this.x = this.destination.x;
-				} else {
-					this.x += this.speed;
-				}
-			} else if (this.x > this.destination.x) {
-				if (this.x - this.destination.x <= this.speed) {
-					this.x = this.destination.x;
-				} else {
-					this.x -= this.speed;
-				}
-			}
-
-			if (this.y < this.destination.y) {
-				if (this.destination.y - this.y <= this.speed) {
-					this.y = this.destination.y;
-				} else {
-					this.y += this.speed;
-				}
-			} else if (this.y > this.destination.y) {
-				if (this.y - this.destination.y <= this.speed) {
-					this.y = this.destination.y;
-				} else {
-					this.y -= this.speed;
-				}
-			}
-		}
-	},
-
 	findPath: function(scene, position) {
 		var graph = scene.sceneGraph();
 
