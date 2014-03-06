650d1bbdf02a79dd976cb859adf6127ddabc0657
David Field <dvf9@aber.ac.uk>
2014-03-06 18:10:06 +0000

    socket.io test - seems to be working

--- app/client/js/game.js
+++ app/client/js/game.js
@@ -6,4 +6,11 @@ var UPDATE_INTERVAL = 1000 / 30;
 
 var engine = new Engine(document.getElementById('game_canvas'));
 
+var socket = io.connect('http://' + document.domain + ':' + location.port + '/test');
+socket.on('test_response', function(message) {
+	console.log("Received: " + message.data);
+});
+
+socket.emit('test_message', {data: 'say'});
+
 window.onload = function() { engine.start() };
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
-from app import app
+from app import app, socketio
 from flask import render_template, send_from_directory
+from flask.ext.socketio import emit
 
 @app.route('/')
 def index():
@@ -8,3 +9,7 @@ def index():
 @app.route('/scene/<path:scene>')
 def scene(scene):
 	return send_from_directory(app.config['SCENES'], scene + '.json')
+
+@socketio.on('test_message', namespace='/test')
+def test_message(message):
+	emit('test_response', {'data': 'what'})
