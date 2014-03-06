1a453f6289d51cf660f9c5d15632ebf57b910ae4
David Field <dvf9@aber.ac.uk>
2014-03-06 17:33:37 +0000

    Setting things up to use SocketIO, and crying while doing it.

---
+++ app/__init__.py
@@ -0,0 +1,15 @@
+import os
+from flask import Flask
+from flask.ext.socketio import SocketIO
+
+app = Flask(__name__, static_folder = 'client')
+
+app.config.update(
+	DEBUG = True,
+	SECRET_KEY = 'dat-secret-key',
+	SCENES = os.path.dirname(__file__) + '/client/data'
+)
+
+socketio = SocketIO(app)
+
+from app import routes
